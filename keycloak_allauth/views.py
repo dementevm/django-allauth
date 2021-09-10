7c5b243b873316190d427826e6f4dc16468357f6
Mikhail <dem.90@list.ru>
2021-09-10 16:25:59 +0300

    initial commit

---
+++ keycloak_allauth/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.urls import reverse
+from django.shortcuts import render
+
+
+def index(request):
+    print(request.user.username)
+    return render(request, 'index.html')
+
+
+def custom_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required
+def secure_test_one(request):
+    return render(request, 'first_secure.html')
+
+
+@login_required
+def secure_test_two(request):
+    return render(request, 'second_secure.html')
